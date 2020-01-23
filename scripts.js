d1fdf491e8e83d947a27fd8b7e194e6b55f2f4d4
Bethan <bethancdavies@gmail.com>
2020-01-23 13:19:23 -0500

    fixed mobile navigation

---
+++ scripts.js
@@ -0,0 +1,15 @@
+  $(function () {
+      $('.portfolioGallery').flickity({
+          wrapAround: true
+      });
+  }); 
+
+const mobileMenu = document.querySelector('.hiddenNav');  
+const navItems = document.querySelectorAll('.navList')
+
+mobileMenu.addEventListener('click', function(){
+    navItems.forEach(item => {
+        item.classList.toggle('clicked') 
+        console.log(item)
+    });
+})
